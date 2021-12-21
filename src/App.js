b0b6133f59aeba8bb936407ed0075a7819c25105
Ivette Fernandez <ivette.diana.fernandez@gmail.com>
2021-12-21 14:59:30 -0500

    added lengthValidator function to SidePanel that turns input text red when reach character limit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import Board from './components/Board';
 import SidePanel from './components/SidePanel'
 
 function App() {
+  // render Board with board id of selected board in dropdown menu
   return (
     <div className="App">
       <header className="App-header">
         <h1>Inspiration Board</h1>
       </header>
       <SidePanel />
+      <Board />
     </div>
   );
 }
--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -1,9 +1,19 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import Board from './Board';
 
 const SidePanel = () => {
     //axios.post for submit buttons for board and card
     // add error logic for going over 40 characters in card's message
+    const lengthValidator = (target) => {
+        const length = document.getElementById(target.id).value.length;
+        if (length === target.maxLength) {
+            target.style.color = 'red';
+        } else {
+            target.style.color = 'black';
+        }
+        console.log('in validator', length);
+    };
+
     // add error logic empty/blank/invalid (too long)/missing "title" or "owner" input
     // add color changing logic for 4 buttons, only in frontend, for card components being displayed
     //axios.delete for delete selected board button
@@ -17,14 +27,14 @@ const SidePanel = () => {
             <h3>Create New Board</h3>
             <form className='BoardForm'>
                 <label>Board Title:</label>
-                <input type='text'></input>
+                <input id='board-title-input' type='text' maxLength='5' onChange={(event) => {lengthValidator(event.target)}}></input>
                 <label>Owner's Name:</label>
-                <input type='text'></input>
+                <input id='owner-name-input' type='text' maxLength='30' onChange={(event) => {lengthValidator(event.target)}}></input>
                 <button>Submit New Board</button>
             </form>
             <h3>Create New Card</h3>
             <form className='CardForm'>
-                <input type='text'></input>
+                <input id='card-message-input' type='text' maxLength='40' onChange={(event) => {lengthValidator(event.target)}}></input>
                 <button>Submit New Card</button>
             </form>
             <h3>Select Color of Cards:</h3>
